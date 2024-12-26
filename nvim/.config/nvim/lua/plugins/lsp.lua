19c52bad6d47c647d1bce533ac864c1676535768
linrswa <jimmy880108@gmail.com>
2024-12-26 09:54:13 +0800

    update lsp.lua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "dockerls" }
+        ensure_installed = { "lua_ls", "dockerls", "pright" },
+        automatic_installation = true
       })
     end
   },
@@ -30,6 +31,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.dockerls.setup({})
+      lspconfig.pyright.setup({})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, {})
     end
