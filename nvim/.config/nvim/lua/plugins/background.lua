ddfb8c04d4b93ab06b5735d439bdbdad64e1ecd4
linrswa <jimmy880108@gmail.com>
2024-12-26 11:21:50 +0800

    change background setting

---
+++ nvim/.config/nvim/lua/plugins/background.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
