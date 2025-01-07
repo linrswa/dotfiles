20dec3bdc5f564fa08651e28ff78afae4f0efdc8
linrswa <jimmy880108@gmail.com>
2025-01-07 13:49:31 +0800

    modify telescope.lua

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,42 +1,48 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'nvim-telescope/telescope-ui-select.nvim'}
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-ui-select.nvim" },
   },
   config = function()
-    local telescope = require('telescope')
-    telescope.setup {
-      pickers = {
-        find_files = {
-          theme = 'ivy'
-        }
-      },
+    local telescope = require("telescope")
+    telescope.setup({
       extensions = {
         fzf = {},
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown()
-        }
-      }
-    }
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      },
+      defaults = {
+        file_ignore_patterns = {
+          ".git/",
+          "node_modules/",
+          "build/",
+          "vcpkg_installed/",
+        },
+      },
+    })
 
-    telescope.load_extension('fzf')
-    telescope.load_extension('ui-select')
+    telescope.load_extension("fzf")
+    telescope.load_extension("ui-select")
 
-    vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<space>fh", builtin.help_tags)
+    vim.keymap.set("n", "<space>ff", builtin.find_files)
+    vim.keymap.set("n", "<space>fb", builtin.buffers)
+    vim.keymap.set("n", "<space>fs", builtin.lsp_document_symbols)
     vim.keymap.set("n", "<space>en", function()
-      require('telescope.builtin').find_files {
-        cwd = vim.fn.stdpath("config")
-      }
+      require("telescope.builtin").find_files({
+        cwd = vim.fn.stdpath("config"),
+      })
     end)
     vim.keymap.set("n", "<space>ep", function()
-      require('telescope.builtin').find_files {
-        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-      }
+      require("telescope.builtin").find_files({
+        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+      })
     end)
-    require "config.telescope.multigrep".setup()
-  end
+    require("config.telescope.multigrep").setup()
+  end,
 }
