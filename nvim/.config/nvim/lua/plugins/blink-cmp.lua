9f48f6af83762889ba5dc953dc534cd606a3886b
linrswa <jimmy880108@gmail.com>
2025-01-08 15:50:17 +0800

    add doc in blink.cmp for code completion

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -10,6 +10,12 @@ return {
         nerd_font_variant = "mono",
       },
       signature = { enabled = true },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 100,
+        },
+      },
     },
   },
 }
