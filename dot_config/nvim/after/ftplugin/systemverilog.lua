ea0795106529dd8a4975e017fe7024627a152337
linrswa <jimmy880108@gmail.com>
2026-03-16 00:13:43 +0800

    add systemverilog ftplugin with indent settings and gd keymap

---
+++ dot_config/nvim/after/ftplugin/systemverilog.lua
@@ -0,0 +1,15 @@
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+
+-- Custom gd for SystemVerilog: search from top of file for first occurrence of <cword>
+vim.keymap.set("n", "gd", function()
+  local word = vim.fn.expand('<cword>')
+  local save_pos = vim.fn.getpos('.')
+  vim.fn.cursor(1, 1)
+  local found = vim.fn.search('\\<' .. vim.fn.escape(word, '\\') .. '\\>', 'cW')
+  if found == 0 then
+    vim.fn.setpos('.', save_pos)
+    vim.notify('No declaration found for: ' .. word, vim.log.levels.WARN)
+  end
+end, { buffer = true, desc = "Go to first occurrence (SystemVerilog)" })
